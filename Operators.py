a219065ba4c9dca15771b1e5d4a0fb9ec7f7deaa
lavanyaraju57 <99262310+lavanyaraju57@users.noreply.github.com>
2022-03-23 10:13:06 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,163 @@
+'''ARITHMETIC OPERATORS'''
+
+# s=float(input("enter the value of s:"))
+# m=float(input("enter the value of m:"))
+# s,m=[int(x) for x in input('enter the value of s and m: ').split()]
+# print(f"the value of {s} + {m} is {s+m}")
+# print(f"the value of {s} - {m} is {s-m}")
+# print(f"the value of {s} * {m} is {s*m}")
+# print(f"the value of {s} / {m} is {s/m}")
+# print(f"the value of {s} % {m} is {s%m}")
+# print(f"the value of {s} ** {m} is {s**m}")
+
+
+'''FLOOR DIVISION'''
+
+# s=5//3
+# print(s)
+# m=5//3.2
+# print(m)
+# k=5.0//3
+# print(k)
+# g=5.0//3.2
+# print(g)
+# b=-5//-3
+# print(b)
+
+
+'''ASSIGNMENT OPERATOR'''
+
+# s=5 ; m=8
+# k=s+m
+# print(k)
+# k=s+s
+# print(k)
+
+# s=8
+# s+=5
+# print(s)
+# s-=2
+# print(s)
+# s-=-5
+# print(s)
+# s*=7
+# print(s)
+# s/=4
+# print(s)
+# s**=6
+# print(s)
+
+# m=int(input("enter the value of m:"))
+# print(f"the value of m is {m}")
+# m+=4
+# print("the value of m after m+=4 is {}".format(m))
+# m-=7
+# print("the value of m after m-=4 is {}".format(m))
+# m*=9
+# print("the value of m after m*=4 is {}".format(m))
+# m/=2
+# print("the value of m after m/=4 is {}".format(m))
+# m**=6
+# print("the value of m after m**=4 is {}".format(m))
+
+
+'''RELATIONAL/COMPARISION OPERATORS'''
+
+# s=int(input("enter the value of s:"))
+# m=int(input("enter the value of m:"))
+# print(f"the value of {s} < {m} is {s<m}")
+# print(f"the value of {s} > {m} is {s>m}")
+# print(f"the value of {s} <= {m} is {s<=m}")
+# print(f"the value of {s} >= {m} is {s>=m}")
+# print(f"the value of {s} == {m} is {s==m}")
+# print(f"the value of {s} != {m} is {s!=m}")
+
+
+# k=int(input("enter the value of k:"))
+# l=int(input("enter the value of l:"))
+# print("the value of %d<%d=%d"%(k,l,k<l))
+# print("the value of k=%d , l=%d and k<l=%d"%(k,l,k<l))
+# print("the value of k=%d , l=%d and k>l=%d"%(k,l,k>l))
+# print("the value of k=%d , l=%d and k<=l=%d"%(k,l,k<=l))
+# print("the value of k=%d , l=%d and k>=l=%d"%(k,l,k>=l))
+# print("the value of k=%d , l=%d and k==l=%d"%(k,l,k==l))
+# print("the value of k=%d , l=%d and k!=l=%d"%(k,l,k!=l))
+
+
+'''LOGICAL OPERATORS'''
+
+# s=int(input("enter the value of s:"))
+# m=int(input("enter the value of m:"))
+# print("the value of s<m and m>s is {}".format(s<m and m>s))
+# print("the value of s>m and m<s is {}".format(s>m and m<s))
+# print("the value of s!=m and m==s is {}".format(s!=m and m==s))
+# print("the value of s==m and m>s is {}".format(s==m or m>s))
+# print("the value of s!=m and m<s is {}".format(s!=m or m<s))
+# print("the opposite of {}=={} is {}".format(s,m, not s==m))
+# print("the opposite of {}!={} is {}".format(s,m, not s!=m))
+
+
+'''BITWISE OPERATOR'''
+
+# s=int(input("enter the value of s:"))
+# m=int(input("enter the value of m:"))
+# k=int(input("enter the value of k:"))
+# print("the value of {} & {} is {}".format(s,m,s&m))
+# print("the value of {} | {} is {}".format(s,m,s|m))
+# print("the value of ~{} is {}".format(s,~s))
+# print("the value of ~{} is {}".format(k,~k))
+
+
+'''IDENTITY OPERATOR'''
+
+# s=[8]; m=8; k=5; l=[8]; n=8; a=k;b=s
+# print(s is k)
+# print(s is m)
+# print(s is l)
+# print(s is a)
+# print(s is b)
+# print(m is n)
+# print(a is b)
+# print(a is not b)
+# print(s is not m)
+# print(m is not l)
+
+
+'''MEMBERSHIP OPERATOR'''
+
+# name=['sandy','madhu','kiran','gagan','bharath','ram','pavan','shiva',]
+# print('madhu' in name)
+# print('gagan' in name)
+# print('shiva' in name)
+# print('kiran' in name)
+# print('sandy' not in name)
+# print('ram' not in name)
+# print('abhi' not in name)
+# print('venky' not in name)
+
+# name.sort()
+# print(name)
+
+# name=['s','k','m','g','r']
+# name.sort()
+# print(*name)
+# name.sort(reverse=True)
+# print(name)
+# for i in name:print(i)
+
+
+'''SHIFT OPERATOR'''
+
+# k=15
+# print(k>>5)
+# print(k>>9)
+# print(k>>1)
+# print(k>>16)
+
+# l=16
+# print(l<<5)
+# print(l<<9)
+# print(l<<1)
+# print(l<<16)
+
+
